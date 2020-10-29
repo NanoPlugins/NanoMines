5c8d752c5374aeabbf866619ea0e3c5baf6b4df8
Diogo <diiiiogosousa.atltic@gmail.com>
2020-10-29 13:49:10 +0000

    Improving numberformat

--- src/main/java/com/nanoplugins/mines/util/NumberFormat.java
+++ src/main/java/com/nanoplugins/mines/util/NumberFormat.java
@@ -2,25 +2,21 @@ package com.nanoplugins.mines.util;
 
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Locale;
 
 public class NumberFormat {
 
-    private final DecimalFormat numberFormat = new DecimalFormat("#,###.##", new DecimalFormatSymbols(new Locale("pt", "BR")));
-    private final List<String> chars = Arrays.asList("", "K", "M", "B", "T", "Q", "QQ", "S", "SS", "O", "N", "D", "UN", "DD", "TR", "QT", "QN", "SD", "SPD", "OD", "ND", "VG", "UVG", "DVG", "TVG", "QTV");
-
+    public final double LOG = 6.907755278982137D;
+    public static final Object[][] VALUES = {
+            {"", "K", "M", "B", "T", "Q", "QQ", "S", "SS", "O", "N", "D", "UN", "DD", "TR", "QT", "QN", "SD", "SPD", "OD", "ND", "VG", "UVG", "DVG", "TVG", "QTV"},
+            {1D, 1000.0D, 1000000.0D, 1.0E9D, 1.0E12D, 1.0E15D, 1.0E18D, 1.0E21D, 1.0E24D, 1.0E27D, 1.0E30D, 1.0E33D, 1.0E36D, 1.0E39D, 1.0E42D, 1.0E45D, 1.0E48D, 1.0E51D, 1.0E54D, 1.0E57D, 1.0E60D, 1.0E63D, 1.0E66D, 1.0E69D, 1.0E72D}
+    };
+    public final DecimalFormat FORMAT = new DecimalFormat("#,###.##", new DecimalFormatSymbols(new Locale("pt", "BR")));
 
     public String formatNumber(double number) {
-        int index = 0;
-        while (number / 1000.0D >= 1.0D) {
-            number /= 1000.0D;
-            index++;
-        }
-
-        String character = index < chars.size() ? chars.get(index) : "";
-        return numberFormat.format(number) + character;
+        if (number == 0) return FORMAT.format(number);
+        int index = (int) (Math.log(number) / LOG);
+        return FORMAT.format(number / (double) VALUES[1][index]) + VALUES[0][index];
     }
 
 }
